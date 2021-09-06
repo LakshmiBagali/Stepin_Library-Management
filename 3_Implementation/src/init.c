0b491e11e1e1cf54a5bfd917a75f6997102a7358
Lakshmi Bagali <lakshmibagali05@gmail.com>
2021-09-06 23:19:50 +0530

    commit

--- 3_Implementation/src/init.c
+++ 3_Implementation/src/init.c
@@ -2,7 +2,7 @@
 
 /* Check the pin matches with default value */
 int init(int pin) {
-	if(pin == 3412 || pin == 1243 ) // condition to check password 
+	if(pin == 3412 || pin == 1243 ) // condition to check the password 
 	    return 1;
 	else
 	    return 0;	
